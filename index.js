958a1e51aa9bb9d99063b0a51777bd187b6e31e0
pvjagadish <47263872+pvjagadish@users.noreply.github.com>
2019-07-29 14:39:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,89 +1,191 @@
-import React from "react";
-import ReactDOM from "react-dom";
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var gCanvas = document.getElementById('gCanvas');
+var imgCount =0;
+var model;
 
-import * as cocoSsd from "@tensorflow-models/coco-ssd";
-import "@tensorflow/tfjs";
-import "./styles.css";
-
-class App extends React.Component {
-  state = {
-    model: null,
-    preview: "",
-    predictions: []
-  };
-
-  componentDidMount() {
-    const video = document.getElementById("video");
-    video.play();
-    cocoSsd.load().then(model => {
-      const canvas = document.getElementById("canvas");
-
-      const ctx = canvas.getContext("2d");
-      canvas.width = video.width;
-      canvas.height = 300;
-      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-      this.detectFrame(video, canvas, model);
-    });
-  }
-
-  detectFrame = (video, canvas, model) => {
-    model.detect(video).then(predictions => {
-      console.log(predictions);
-      requestAnimationFrame(() => {
-        this.detectFrame(video, canvas, model);
-      });
-    });
-  };
-
-  onImageChange = e => {
-    const c = document.getElementById("canvas");
-    const ctx = c.getContext("2d");
-    this.cropToCanvas(e.target, c, ctx);
-    this.state.model.detect(c).then(predictions => {
-      // Font options.
-      const font = "16px sans-serif";
-      ctx.font = font;
-      ctx.textBaseline = "top";
-
-      predictions.forEach(prediction => {
-        const x = prediction.bbox[0];
-        const y = prediction.bbox[1];
-        const width = prediction.bbox[2];
-        const height = prediction.bbox[3];
-        // Draw the bounding box.
-        ctx.strokeStyle = "#00FFFF";
-        ctx.lineWidth = 4;
-        ctx.strokeRect(x, y, width, height);
-        // Draw the label background.
-        ctx.fillStyle = "#00FFFF";
-        const textWidth = ctx.measureText(prediction.class).width;
-        const textHeight = parseInt(font, 10); // base 10
-        ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
-      });
-
-      predictions.forEach(prediction => {
-        const x = prediction.bbox[0];
-        const y = prediction.bbox[1];
-        // Draw the text last to ensure it's on top.
-        ctx.fillStyle = "#000000";
-        ctx.fillText(prediction.class, x, y);
-      });
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <video id="video" width="400" height="300" controls>
-          <source crossorigin="anonymous" src="/mov_bbb.mp4" type="video/mp4" />
-          Your browser does not support HTML5 video.
-        </video>
-        <canvas id="canvas" controls />
-      </div>
-    );
-  }
+function isAndroid() {
+  return /Android/i.test(navigator.userAgent);
 }
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+function isiOS() {
+  return /iPhone|iPad|iPod/i.test(navigator.userAgent);
+}
+
+function isMobile() {
+  return isAndroid() || isiOS();
+}
+      
+const videoWidth = 500;
+const videoHeight = 500;
+const color = 'aqua';
+
+async function setupCamera() {
+  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+    alert(
+        'Browser API navigator.mediaDevices.getUserMedia not available');
+  }
+
+  const video = document.getElementById('video');
+
+  const mobile = isMobile();
+  try{
+  const stream = await navigator.mediaDevices.getUserMedia({
+    'audio': false,
+    'video': {
+      facingMode: 'user',
+      width: mobile ? undefined : videoWidth,
+      height: mobile ? undefined : videoHeight,
+    },
+  });
+  
+  video.srcObject = stream;
+}catch(err){alert(err)}
+  return new Promise((resolve) => {
+    video.onloadedmetadata = () => {
+      resolve(video);
+    };
+  });
+}
+
+async function loadVideo() {
+  const video = await setupCamera();
+  loadModel();
+  video.play();
+  render();
+  
+}	   	    
+function drawPoint(ctx, y, x, r, color) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
+async function render()
+{
+    count = 0;
+    grace = 0;
+    let oldX;
+    let oldY;
+    var $this = video; //cache
+    (function loop() {
+      if (!$this.paused && !$this.ended) {
+        drawCanvas($this);
+        setTimeout(loop, 30); // drawing at 30fps
+      }
+    })();
+}
+
+function drawCanvas($this)
+{
+    ctx.clearRect(0, 0, videoWidth, videoHeight); 
+    ctx.save();
+    ctx.scale(-1, 1);
+    ctx.translate(-videoWidth, 0);
+    ctx.drawImage($this, 0, 0, videoWidth, videoHeight);
+    ctx.restore();
+}
+
+function startPred()
+{
+  (function loop() {
+    const imgData = getImageData();
+    const pred = predict(imgData);
+    tf.toPixels(pred, gCanvas);
+    setTimeout(loop, 30); // drawing at 30fps
+  })();
+  const imgData = getImageData();
+  const pred = predict(imgData);
+  tf.toPixels(pred, gCanvas);
+}
+/*
+get the current image data 
+*/
+function getImageData() {
+    //get image data according to dpi 
+    const dpi = window.devicePixelRatio    
+    const x = 0 
+    const y = 0 
+    const w = canvas.width * dpi 
+    const h = canvas.height * dpi 
+    const imgData = ctx.getImageData(x, y, w, h)
+    return imgData
+}
+
+/*
+get the prediction 
+*/
+function predict(imgData) {
+    return tf.tidy(() => {
+        //get the prediction 
+        const gImg = model.predict(preprocess(imgData))
+        //post process 
+        const postImg = postprocess(gImg)
+        return postImg   
+    })
+}
+
+/*
+preprocess the data
+*/
+function preprocess(imgData) {
+    return tf.tidy(() => {
+        //convert to a tensor 
+        const tensor = tf.fromPixels(imgData).toFloat()
+
+        //resize 
+        const resized = tf.image.resizeBilinear(tensor, [256, 256])
+                
+        //normalize 
+        const offset = tf.scalar(127.5);
+        const normalized = resized.div(offset).sub(tf.scalar(1.0));
+
+        //We add a dimension to get a batch shape 
+        const batched = normalized.expandDims(0)
+        
+        return batched
+    })
+}
+
+/*
+post process 
+*/
+function postprocess(tensor){
+     const w = gCanvas.width  
+     const h = gCanvas.height 
+     
+     return tf.tidy(() => {
+        //normalization factor  
+        const scale = tf.scalar(0.5);
+        
+        //unnormalize and sqeeze 
+        const squeezed = tensor.squeeze().mul(scale).add(scale)
+
+        //resize to canvas size 
+        const resized = tf.image.resizeBilinear(squeezed, [w, h])
+        return resized
+    })
+}
+
+/*
+load the model
+*/
+async function loadModel() {
+    //load the model 
+    model = await tf.loadModel("model/model.json");
+
+    //warm up 
+    model.predict(tf.zeros([1, 256, 256, 3]))
+
+    //enable prediction 
+    $('#start').prop('disabled', false);
+}
+
+function start()
+{
+  loadVideo();
+  loadModel();
+}
+
+start();
